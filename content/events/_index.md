5680eff7b6fbb0edb32e5f14dff613a8607799b8
Rob Hough <robhough180@gmail.com>
2020-03-18 17:17:23 +0000

    Added Events page

---
+++ content/events/_index.md
@@ -0,0 +1,13 @@
+---
+title: "Events & Web Classes"
+description: "Learn remotely from our design & innovation experts, we can equip you with all the tools you need to solve critical problems, future proof your career & transform the way you work."
+date: 2020-01-01
+menu: main
+name: "Events"
+weight: 3
+draft: false
+draft: false
+---
+
+
+
--- themes/agency-theme/src/scripts/main.js
+++ themes/agency-theme/src/scripts/main.js
@@ -3,8 +3,9 @@ const $mainNavToggle = document.getElementById('toggle-nav-links');
 const $cookieBar = document.getElementById('cookie-bar');
 const $gaAccept = document.getElementById('ga-accept');
 const $gaOptout = document.getElementById('ga-optout');
+const $eventList = document.getElementById('event-list');
 
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', () => {
 
   if (document.cookie.indexOf('seenCookieBar=true') < 0) {
     $cookieBar.classList.add('is-visible');
@@ -20,7 +21,7 @@ document.addEventListener('DOMContentLoaded', function () {
     gaOptout();
   })
 
-  $mainNavToggle.addEventListener('click', function () {
+  $mainNavToggle.addEventListener('click', () => {
     if ($mainNav.classList.contains('is-open')) {
       $mainNav.classList.remove('is-open');
       $mainNavToggle.innerHTML = 'Menu';
@@ -28,5 +29,78 @@ document.addEventListener('DOMContentLoaded', function () {
     }
     $mainNav.classList.add('is-open');
     $mainNavToggle.innerHTML = 'Close';
-  })
-})
+  });
+
+  const createElement = (type, target) => {
+    let node = document.createElement(type);
+    return target.appendChild(node);
+  }
+
+
+  fetch('http://fast-eyrie-12998.herokuapp.com/api/strike')
+    .then(
+      function (response) {
+        if (response.status !== 200) {
+          console.log('Looks like there was a problem. Status Code: ' +
+            response.status);
+          return;
+        }
+        // Examine the text in the response
+        response.json().then(function (data) {
+          const spinner = document.querySelector('.loading-spinner');
+          const events = data.events;
+
+          if (events) {
+            spinner.parentNode.removeChild(spinner);
+          }
+          events.forEach(event => {
+            const { id, start, end, name, url, is_free, published, status } = event;
+
+            // Eventbrite pulls all events regardless of being live or not
+            if (!published || status != 'live') {
+              return false
+            }
+
+            // Create element
+            let el = createElement('li', $eventList);
+            el.innerHTML = `
+              <p><strong>${name.text}</strong></p>
+              <div class="event-time">              
+                <p><strong>${dayjs(start.local).format('ddd DD MMM')}</strong></p>
+                <p>${dayjs(start.local).format('HH:mm')} - ${dayjs(end.local).format('HH:mm')}</p>
+              </div>
+              <p class="event-price ${is_free ? null : 'event-price-cost'}">${is_free ? '<strong>Free</strong>' : 'Loading...'}</p>
+              <div class="event-button-wrap">
+                <a href="${url}" class="button button-secondary">Register</a>
+              </div>
+            `;
+
+            if (!is_free) {
+              fetch(`http://fast-eyrie-12998.herokuapp.com/api/strike/tickets?event=${id}`)
+                .then(response => {
+                  if (response.status !== 200) {
+                    return
+                  }
+                  response.json().then(data => {
+                    let element = document.querySelectorAll('.event-price-cost')
+                    element.forEach(el => {
+                      el.innerHTML = `<strong>${data.ticket_classes[0].cost.display}</strong>`
+                    })
+
+                  });
+                })
+                .catch(function (err) {
+                  console.log('Fetch Error :-S', err);
+                });
+            }
+          });
+        });
+      }
+    )
+    .catch(function (err) {
+      console.log('Fetch Error :-S', err);
+    });
+
+
+});
+
